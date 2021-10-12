59cb15c16174db32bcdd1d1cec536c31f0a7c713
Dybol <mdyblik01@gmail.com>
2021-10-12 10:42:32 +0200

    Initial commit

---
+++ src/main/java/me/mikolaj/messageboard/SimpleMessageBoardApplication.java
@@ -0,0 +1,13 @@
+package me.mikolaj.messageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMessageBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMessageBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mikolaj/messageboard/SimpleMessageBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mikolaj.messageboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMessageBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
