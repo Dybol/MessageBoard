4c935745f862eb9131600f7cac284bacfede21ab
Dybol <mdyblik01@gmail.com>
2021-11-29 23:18:34 +0100

    Add unit tests

---
+++ src/test/java/me/mikolaj/messageboard/UserRepositoryTest.java
@@ -0,0 +1,17 @@
+package me.mikolaj.messageboard;
+
+import me.mikolaj.messageboard.user.User;
+import me.mikolaj.messageboard.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentServiceTest {
+
+	@Autowired
+	private UserService userService;
+
+	public void check() {
+		User user
+		userService.
+				userService.findByEmail()
+	}
+}
